0604241122fa48e61fcc673f5d49a70b892efeec
stephanieonyekaba <onyek009@umn.edu>
2022-08-23 17:15:04 -0400

    m

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/portfolio/stephanieonyekaba.github.io.git";
+const repoURL = "https://github.com/stephanieonyekaba/portfolio2";
 
 ghpages.publish(
   pathname,
